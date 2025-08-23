f382067d884e8e5603a7251206e09e401ebfd170
MohamedIbnKhaled <mohamedrobot1@gmail.com>
2025-08-23 19:09:43 +0300

    feat: add Student entity

---
+++ src/main/java/com/Spectro_Systems/Student/Management/model/Student.java
@@ -0,0 +1,29 @@
+package com.Spectro_Systems.Student.Management.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+    private LocalDate dateOfBirth;
+
+
+}
